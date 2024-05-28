f86301e6cc9ad04ac3241871662a34faf027d797
RodrigoLV9 <rodrigolv.975@gmail.com>
2024-05-28 11:38:20 -0300

    Rename main.html to index.html for GitHub pages

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const code=document.getElementById("code");
+const button_run=document.getElementById("button-run");
+
+button_run.addEventListener('click',(e)=>{
+    console.log(code.value)
+    let valueCode=code.value
+    
+    /* if(valor.includes("pintarRojo")){
+        console.log(`intruccion: ${valor} detectado`)
+    }else{
+        console.log('No se ha detectado nada')
+    } */
+})
+
+
+
+
